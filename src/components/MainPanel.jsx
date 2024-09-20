c4e146cf4aeb71f2f5721c550e6453942a98dd8d
MilkTea24 <kimmanjae15@pusan.ac.kr>
2024-09-20 15:39:01 +0900

    feat: 데이터 없을 때 대체 화면 출력 및 MainPanel 기타 수정

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -25,11 +25,19 @@ const MainPanel = ({ keyValuePairs, queryResults }) => {
 
         const newQueryTableHeight = 95 - newDataTableHeight; // QueryTable 높이 계산 (백분율)
 
-
-
         setDataTableHeight(newDataTableHeight);
         setQueryTableHeight(newQueryTableHeight);
     };
+
+    //마우스 놓을 때 크기 변경
+    const mouseMoveResize = (e) => {
+        e.preventDefault();
+
+        // 마우스 이동 이벤트로 높이 조정
+        document.addEventListener('mousemove', handleResize);
+        document.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', handleResize);
+        })};
 
     return (
         <main className="w-9/12 h-screen bg-slate-900">
@@ -38,22 +46,22 @@ const MainPanel = ({ keyValuePairs, queryResults }) => {
                 <div
                     style={{height: `${dataTableHeight}%`}}
                 >
-                    <DataTable keyValuePairs={keyValuePairs} height={dataTableHeight}/>
-                </div>
+                    {keyValuePairs, keyValuePairs.length > 0 ? (
+                        <DataTable keyValuePairs={keyValuePairs} height={dataTableHeight}/>
+                        ) : (
+                    <div className="flex flex-col justify-center items-center h-full">
+                        <img src="/box_1011831.png" alt="No data" className="w-48 h-48 mb-1"/>
+                        <p className="text-gray-600 text-2xl">검색 결과를 찾을 수 없어요</p>
+                    </div>
+                )}
 
-                {/* 리사이즈 핸들러 */}
-                <div
-                    className="bg-gray-900 cursor-row-resize z-20 relative"
-                    style={{height: 5}}
-                    onMouseDown={(e) => {
-                        e.preventDefault();
-                        // 마우스 이동 이벤트로 높이 조정
+            </div>
 
-                        document.addEventListener('mousemove', handleResize);
-                        document.addEventListener('mouseup', () => {
-                            document.removeEventListener('mousemove', handleResize);
-                        });
-                    }}
+            {/* 리사이즈 핸들러 */}
+            <div
+                className="bg-gray-900 cursor-row-resize z-20 relative"
+                style={{height: 5}}
+                onMouseDown={mouseMoveResize}
                 />
 
                 {/* QueryTable */}
